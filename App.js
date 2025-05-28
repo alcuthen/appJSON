e76910c3c8fe707fbf9806ec4cee3e7be663c8fb
alcuthen <alcuthen@gmail.com>
2025-05-28 09:09:17 -0300

    tentativa_2

--- App.js
+++ App.js
@@ -7,19 +7,21 @@ import Students from './src/Students';
 const [students, setStudents] = useState([]);
 
 useEffect(() => {
-  try {
+  
     async function loadStudents() {
+    try {
       const response = await api.get("baseJSON/master/datapp.json");
       // console.log(response.data);
       setStudents(response.data);
     }
+  
+    catch (e) {
+      console.error(e);
   }
-  catch (e) {
-    console.error(e);
-  }
+}
   loadStudents();
 },
-  [])
+[]);
 
 
 
